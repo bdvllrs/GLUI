0bb97a58018b8f4f2bd3b3abdb41baf3ff21354a
bdvllrs <bdvllrs@gmail.com>
2017-08-20 23:34:16 +0200

    webpack configuration file

--- src/index.js
+++ src/index.js
@@ -1,6 +1,3 @@
-import './scss/open-iconic.scss';
-import './scss/normalize.css';
-import './scss/app.scss';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './js/components/App';
--- webpack.config.js
+++ webpack.config.js
@@ -1,55 +1,101 @@
 let path = require('path');
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const UglifyJSPlugin = require("uglifyjs-webpack-plugin");
+const CleanWebpackPlugin = require("clean-webpack-plugin");
+const dev = process.argv.indexOf('-p') < 0;
 
-const extractSass = new ExtractTextPlugin({
-    filename: "css/[name].css"
-});
+let cssLoaders = [
+    {loader: 'css-loader', options: { minimize: !dev }}
+];
 
-module.exports = {
+if (!dev) {
+    cssLoaders.push({
+        loader: 'postcss-loader',
+        options: {
+            plugins: (loader) => [
+                require('autoprefixer')({
+                    browsers: ['last 2 versions', 'ie > 8']
+                })
+            ]
+        }
+    });
+}
+
+let config = {
     entry: {
-        app: './src/index.js'
+        app: ['./src/index.js', './src/scss/app.scss']
     },
     output: {
-        filename: 'js/app.js',
-        path: path.resolve(__dirname, 'dist')
+        filename: '[name].js',
+        path: path.resolve(__dirname, './public/assets'),
+        publicPath: "/assets/"
     },
     module: {
         rules: [
             {
                 test: /\.(js|jsx)$/,
-                use: 'babel-loader'
+                exclude: /(node_modules|bower_components)/,
+                use: ['babel-loader']
             },
             {
-                test: /\.(png|svg|jpg)$/,
-                use: [
-                    'file-loader?name=[name].[ext]&outputPath=images/'
-                ]
-            },
-            {
-                test: /\.(scss|css)$/,
-                use: extractSass.extract({
-                    use: [{
-                        loader: "css-loader"
-                    }, {
-                        loader: "sass-loader",
-                        options: {
-                            includePaths: [path.resolve(__dirname, 'src/scss')]
-                        }
-                    }],
-                    fallback: "style-loader"
+                test: /\.css$/,
+                use: ExtractTextPlugin.extract({
+                    use: cssLoaders,
+                    fallback: 'style-loader',
+                    publicPath: '/assets'
                 })
             },
             {
-                test: /\.(woff|woff2|eot|ttf|otf)$/,
+                test: /\.scss$/,
+                use: ExtractTextPlugin.extract({
+                    use: [
+                        ...cssLoaders,
+                        {
+                            loader: "sass-loader",
+                            options: {
+                                includePaths: [path.resolve(__dirname, 'src/scss')]
+                            }
+                        }
+                    ],
+                    fallback: "style-loader",
+                    publicPath: '/assets'
+                })
+            },
+            {
+                test: /\.(png|svg|jpe?g|woff2?|eot|ttf|otf)$/,
                 use: [
-                    'file-loader?name=[name].[ext]&publicPath=../fonts/'
+                    {
+                        loader: 'url-loader',
+                        options: {
+                            limit: 8192,
+                            name: '[name].[ext]',
+                            publicPath: '/assets/',
+                        }
+                    }
                 ]
             },
         ]
     },
-    devtool: 'inline-source-map',
+    devtool: dev ? 'cheap-module-eval-source-map ' : false,
+    devServer: {
+        contentBase: path.resolve('./public'),
+    },
     plugins: [
-        extractSass
+        new ExtractTextPlugin({
+            filename: "css/[name].css",
+            disable: dev,
+        })
     ]
 };
 
+if (!dev) {
+    config.plugins.unshift(new UglifyJSPlugin());
+    config.plugins.push(new CleanWebpackPlugin(['public'], {
+        exclude: ['index.html'],
+        root: path.resolve('./'),
+        verbose: true,
+        dry: false
+    }))
+}
+
+module.exports = config;
